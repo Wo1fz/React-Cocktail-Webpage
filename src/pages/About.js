685412b5ec14a6bfc07743fda0b3e3250b04e268
Wo1fz <nmy_97@hotmail.com>
2021-06-20 18:12:40 +0800

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque dolorum
+        porro temporibus accusamus quibusdam quam consectetur minus! Unde qui
+        temporibus at repellat amet quibusdam ipsa doloremque officia ipsam
+        numquam illum veritatis fuga nostrum possimus, neque mollitia odit culpa
+        laboriosam, aperiam, sequi vel deserunt velit vitae! Accusamus, dolorem
+        provident. Quidem, fugiat.
+      </p>
+    </section>
+  )
+}
+
+export default About
